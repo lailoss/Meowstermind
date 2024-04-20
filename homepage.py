13bfd74b9c1b98179d546d37259a4f3c7bcff7ea
vidhya dariney <vidhiney05@gmail.com>
2024-04-20 20:25:33 +0800

    added image

--- homepage.py
+++ homepage.py
@@ -1,3 +1,11 @@
 from tkinter import *
 root = Tk()
+#root.geometry("1920x1080")
 
+#background image
+canvas= Canvas(root, width=192, height="108")
+canvas.pack()
+canvas.create_image(0,0, image= PhotoImage(file= "#3 bedroom.#3 bedroom.jpg"))
+
+#important loop-----------------------
+root.mainloop()
