ad5ab65ed3683eda411fc9faa5808db8b7892846
vidhya dariney <vidhiney05@gmail.com>
2024-04-20 20:55:36 +0800

    cleaned the pic

--- homepage.py
+++ homepage.py
@@ -3,10 +3,10 @@ root = Tk()
 #root.geometry("1920x1080")
 
 #background image
-canvas= Canvas(root, width=1920, height="1080")
+backgroundimage= PhotoImage(file = "bg_3_bedroom.png")
+canvas= Canvas(root, width=backgroundimage.width(), height=backgroundimage.height())
 canvas.pack()
-backgroundimage= PhotoImage(file = "bg_3_bedroom.jpg")
-canvas.create_image(0,0, image= backgroundimage)
+canvas.create_image(0,0, anchor=NW, image= backgroundimage)
 
 #important loop-----------------------
 root.mainloop()
