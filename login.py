f4ed2213fa26c29b51b1e48b1981485c43eea9c0
vidhya dariney <vidhiney05@gmail.com>
2024-04-28 21:44:56 +0800

    FINALLY CONNECTED REGISTER TO LOGIN

--- login.py
+++ login.py
@@ -1,56 +1,57 @@
 from tkinter import *
 from tkinter import messagebox
-import os
-root = Tk()
-root.geometry("1200x700")
-root.title("Login Page")
-root.configure(bg="#E8D09C")
 
 
-#FUNCTIONS ---------------------------------------------------------
-def login():
-    username="meowstermind" #link db
-    password="12345" #link db
-    if username_entry.get()==username and password_entry.get()==password:
-         os.system("homescreen.py")
-    else:
-        messagebox.showerror(title="Error", message="You shall not pass.")
+def create_LOGwindow():
+    LOGwindow = Tk()
+    LOGwindow.geometry("1200x700")
+    LOGwindow.title("Login Page")
+    LOGwindow.configure(bg="#E8D09C")
 
 
-#PARAMETER-----------------------------------------------------------
-font_30 = ("Gill Sans MT", 30, "bold")
-font_20 = ("Gill Sans MT", 20)
-font_15 = ("Gill Sans MT", 15)
+    #FUNCTIONS ---------------------------------------------------------
+    def login():
+        username="meowstermind" #link db
+        password="12345" #link db
+        if username_entry.get()==username and password_entry.get()==password:
+            os.system("homescreen.py")
+        else:
+            messagebox.showerror(title="Error", message="You shall not pass.")
 
 
-#FRAME---------------------------------------------------------------
-frame=Frame(root, bg="#FFFFFF", padx=20, pady=20)
+    #PARAMETER-----------------------------------------------------------
+    font_30 = ("Gill Sans MT", 30, "bold")
+    font_20 = ("Gill Sans MT", 20)
+    font_15 = ("Gill Sans MT", 15)
 
 
-#WIDGETS-------------------------------------------------------------
-login_title = Label(frame, text="L O G I N", font=font_30, padx=0, pady=30, bg="#FFFFFF")
-username_label = Label(frame, text="Username", font=font_15, padx=30, pady=15, bg="#FFFFFF")
-username_entry = Entry(frame, font=font_15, bg="#FFFFFF")
-username_entry.get()
-password_label = Label(frame, text="Password", font=font_15, padx=30, pady=15, bg="#FFFFFF")
-password_entry = Entry(frame, show="•", font=font_15, bg="#FFFFFF")
-password_entry.get()
-
-register_button = Button(frame, text = "Don't have an account? Register now!", font=font_15, fg= "blue", bg= "#FFFFFF" ,relief="flat") #change to hyperlink
-
-login_button = Button(frame, text="Login", font=font_20, bg="#FFFFFF", relief= "flat", padx=50, command=login)
+    #FRAME---------------------------------------------------------------
+    frame=Frame(LOGwindow, bg="#FFFFFF", padx=20, pady=20)
 
 
-#pack it in
-login_title.grid(row=0, column=1, columnspan=2, sticky="ew")
-username_label.grid(row=1, column=1)
-username_entry.grid(row=1, column=2)
-password_label.grid(row=2, column=1)
-password_entry.grid(row=2, column=2)
-register_button.grid(row= 3, column= 1, columnspan=2, sticky="ew")
-login_button.grid(row=4, column=1, columnspan=2)
+    #WIDGETS-------------------------------------------------------------
+    login_title = Label(frame, text="L O G I N", font=font_30, padx=0, pady=30, bg="#FFFFFF")
+    username_label = Label(frame, text="Username", font=font_15, padx=30, pady=15, bg="#FFFFFF")
+    username_entry = Entry(frame, font=font_15, bg="#FFFFFF")
+    username_entry.get()
+    password_label = Label(frame, text="Password", font=font_15, padx=30, pady=15, bg="#FFFFFF")
+    password_entry = Entry(frame, show="•", font=font_15, bg="#FFFFFF")
+    password_entry.get()
 
-frame.pack(side="top", expand=True)
+    register_button = Button(frame, text = "Don't have an account? Register now!", font=font_15, fg= "blue", bg= "#FFFFFF" ,relief="flat") #change to hyperlink
+
+    login_button = Button(frame, text="Login", font=font_20, bg="#FFFFFF", relief= "flat", padx=50, command=login)
 
 
-root.mainloop()
+    #pack it in
+    login_title.grid(row=0, column=1, columnspan=2, sticky="ew")
+    username_label.grid(row=1, column=1)
+    username_entry.grid(row=1, column=2)
+    password_label.grid(row=2, column=1)
+    password_entry.grid(row=2, column=2)
+    register_button.grid(row= 3, column= 1, columnspan=2, sticky="ew")
+    login_button.grid(row=4, column=1, columnspan=2)
+    frame.pack(side="top", expand=True)
+
+
+    LOGwindow.mainloop()
--- register.py
+++ register.py
@@ -1,34 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
-import os
-root = Tk()
-root.geometry("1200x700")
-root.title("Registration")
-root.configure(bg="#E8D09C")
+import login
+REGwindow = Tk()
+REGwindow.geometry("1200x700")
+REGwindow.title("Registration")
+REGwindow.configure(bg="#E8D09C")
 
 
 #FUNCTIONS ---------------------------------------------------------
-def validate_password():
-    password = password_entry.get()
-    repassword = repassword_entry.get()
+def register():
+    password = password_entry.get().strip()
+    repassword = repassword_entry.get().strip()
     if len(password) < 8:
         messagebox.showerror("Error", "Password must be at least 8 characters long.")
         return False
-    if password != repassword:
+    elif password != repassword:
         messagebox.showerror("Error", "Passwords do not match.")
         return False
-    else:
-        return True
-        create_button.config(state=NORMAL)
-
-def register():
-    print("Register function called")
-    if validate_password():
-        print("Password validation passed")
+    else: 
         messagebox.showinfo("Success", "Account created successfully!")
-        os.system(login.py)
-    else:
-        print("Password validation failed")
+        login.create_LOGwindow()
+        REGwindow.withdraw()
 
 
 #PARAMETER-----------------------------------------------------------
@@ -38,36 +30,43 @@ font_15 = ("Gill Sans MT", 15)
 
 
 #FRAME---------------------------------------------------------------
-frame=Frame(root, bg="#FFFFFF", padx=20, pady=20)
+frame=Frame(REGwindow, bg="#FFFFFF", padx=20, pady=20)
 
 
 #WIDGETS-------------------------------------------------------------
 register_title = Label(frame, text="REGISTRATION", font=font_30, padx=0, pady=30, bg="#FFFFFF")
+
 username_label = Label(frame, text="Username", font=font_15, pady=5, bg="#FFFFFF")
 username_entry = Entry(frame, font=font_15, bg="#FFFFFF")
-username_entry.get()
+
 password_label = Label(frame, text="Password", font=font_15, pady=5, bg="#FFFFFF")
 password_entry = Entry(frame, show="•", font=font_15, bg="#FFFFFF")
+
 repassword_label = Label(frame, text="Re-enter Password", font=font_15, pady=5, bg="#FFFFFF")
 repassword_entry = Entry(frame, show="•", font=font_15, bg="#FFFFFF")
-repassword_entry.get() #uuuuuuuuuuuuuuuuuuuuuu
 
 register_button = Button(frame, text = "Already have an account? Login now!", font=font_15, fg= "navy", bg= "#FFFFFF" ,relief="flat") #change to hyperlink
-
 create_button = Button(frame, text="Create Account", font=font_20, bg="#FFFFFF", borderwidth=0, padx=50, command=register)
 
 
-#pack it in
+#BINDERS--------------------------------------------------------------
+
+
+#PACK IT IN (OR GRID IG)----------------------------------------------
 register_title.grid(row=0, column=1, columnspan=2, sticky="ew")
+
 username_label.grid(row=1, column=1)
 username_entry.grid(row=1, column=2)
+
 password_label.grid(row=2, column=1)
 password_entry.grid(row=2, column=2)
+
 repassword_label.grid(row=3, column=1)
 repassword_entry.grid(row=3, column=2)
+
 create_button.grid(row=4, column=1, columnspan=2)
 register_button.grid(row= 5, column= 1, columnspan=2, sticky="ew")
 
 frame.pack(side="top", expand=True)
 
-root.mainloop()
+REGwindow.mainloop()
