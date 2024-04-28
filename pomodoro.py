b1a237ef9aa5973f694a65fefb0bce1ed452d98b
hanani <hhanani.iman@gmail.com>
2024-04-28 21:47:58 +0800

    added popup box

--- pomodoro.py
+++ pomodoro.py
@@ -3,6 +3,7 @@ from tkinter import *
 import pygame
 from tkinter import font
 import time
+from tkinter import messagebox
 
 pygame.init()
 
@@ -16,6 +17,7 @@ root.configure(bg='red')
 root.iconbitmap(r'./images/pomodoroIcon.ico')
 canvas = Canvas()
 root.resizable(False, False)
+pygame.mixer.init()
 
 #consts
 red='#d04e2f'
@@ -33,47 +35,86 @@ bg.pack()
 
 hrs = StringVar(root, value='00')
 hrs.set("00")
-Entry(root, textvariable=hrs, fg='black', width=2, font='arial 40', borderwidth=2, ). place(x='100',y='80')
+Entry(root, textvariable=hrs, fg='black', width=2, font='arial 40', borderwidth=0, ). place(x='100',y='80')
 Label(root, text='HOURS', bg='white').place(x='180',y='120')
 
 
 mins=StringVar(root, value='00')
-Entry(root, textvariable=mins, width='2',fg='black',  font='arial 40',borderwidth=2).place(x='250',y='80')
+Entry(root, textvariable=mins, width='2',fg='black',  font='arial 40',borderwidth=0).place(x='250',y='80')
 Label(root,text='MINS', bg='white').place(x='330',y='120')
 mins.set("00")
 
 sec=StringVar(root, value='00')
-Entry(root, textvariable=sec, width=2,  fg='black', font='arial 40',borderwidth=2, bg='white'). place(x='400',y='80')
+Entry(root, textvariable=sec, width=2,  fg='black', font='arial 40',borderwidth=0, bg='white'). place(x='400',y='80')
 Label(root,text='SEC', bg='white').place(x='470',y='120')
 sec.set("00")
 
+
+current_time=None
+
 def timer():
+    global time_run
+    global current_time
     total_time = int(hrs.get()) * 3600 + int(mins.get()) * 60 + int(sec.get())
     
     if total_time > 0:
+        
         total_time -= 1
-        hrs.set(str(total_time // 3600).zfill(2))
-        mins.set(str((total_time % 3600) // 60).zfill(2))
-        sec.set(str(total_time % 60).zfill(2))
-        root.after(1000, timer)
-    else:
-        pygame.mixer.Sound.play(beep)
-
-#play, pause and stop buttons
-def start_timer():
-    timer()
+        hrs.set(str(total_time // 3600).zfill(1))
+        mins.set(str((total_time % 3600) // 60).zfill(1))
+        sec.set(str(total_time % 60).zfill(1))
+        current_time= root.after(1000, timer) #starts countdown
     
-def pause_timer():
-    hrs.set()
+    else:
+        pygame.mixer.music.play(beep)
 
+
+def start_timer():
+    global time_run
+    time_run=True
+    timer()
+   
+def pause_timer():
+    global time_run
+    time_run=False
+    print('timer has paused')
+    global current_time
+    if current_time is not None:
+        root.after_cancel(current_time) #cancels the operation above
+   
+    global pause_popup
+    global pausebg
+    pausebg=PhotoImage(file=r'C:\Users\USER\Projects\Meowstermind\pausepopup.png')
+    pause_popup = Toplevel()
+    pause_popup.title(f'Don\'t give up!')
+    pause_popup.geometry('300x200')
+    pause_popup.resizable(False,False)
+    pause_popup.iconbitmap(r'./images/sad.ico')
+    pause_popupbg=Label(pause_popup,image=pausebg)
+    pause_popupbg.pack()
+    pausemsg=Label(pause_popup, text='DON’T STOP UNTIL YOU’RE PROUD.', font=('arial 10 bold'), bg='#FAF9F7')
+    pausemsg.place(x='35', y='70')
+    pause_yes=Label(pause_popup, 'Continue?', font=('calibri 10 bold'))
+    pause_yes.place(x='50', y='70')
+    
+    
+  
+   
+   
+def stop_timer():
+    global time_run
+    time_run=False
+    hrs.set('00')
+    mins.set('00')
+    sec.set('00')
 
 #play, pause and stop buttons
 starticon=PhotoImage(file=r'C:\Users\USER\Projects\Meowstermind\images\start.png')
 startbutton=Button(root, text='start', image=starticon,bg='white', borderwidth=0,command=start_timer). place(x='260',y=' 230')
 pauseicon=PhotoImage(file=r'C:\Users\USER\Projects\Meowstermind\images\pause.png')
-pausebutton=Button(root, text='pause', image=pauseicon, bg='white', borderwidth=0).place(x='200',y='250')
+pausebutton=Button(root, text='pause', image=pauseicon, bg='white', borderwidth=0, command=pause_timer).place(x='200',y='250')
 stopicon=PhotoImage(file=r'C:\Users\USER\Projects\Meowstermind\images\stop.png')
-stopbutton=Button(root,text='stop', image=stopicon, bg='white', borderwidth=0).place(x='350',y='250')
+stopbutton=Button(root,text='stop', image=stopicon, bg='white', borderwidth=0, command=stop_timer).place(x='350',y='250')
 
 
 #time presets
