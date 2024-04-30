5174098a049ed7cf79e1d9bcc07c16594b7b419a
vidhya dariney <vidhiney05@gmail.com>
2024-04-30 22:49:30 +0800

    made an 'editor', so far it only autofills the entry boxes to be edited

--- testdatabase.py
+++ testdatabase.py
@@ -20,7 +20,7 @@ c.execute("""CREATE TABLE accounts(
 
 #FUNCTIONS---------------------------------------------------------
 
-def submit(): #submit function
+def submit(): 
     conn = sqlite3.connect("account.db") #create / fetch database
     c = conn.cursor() #create cursor
 
@@ -75,6 +75,40 @@ def delete():
     password.delete(0, END)
 
 
+def edit():
+    editor = Tk()
+    editor.title("Edit Record")
+    editor.geometry("500x500")
+
+    conn = sqlite3.connect("account.db") #create / fetch database
+    c = conn.cursor() #create cursor
+
+    record_id = delete_box.get()
+    c.execute("SELECT * FROM accounts WHERE oid =" + record_id)
+    records = c.fetchall()
+
+    #REPEAT INPUTS--------------------------------------------
+    unlabel_editor = Label(editor, text="Username", pady=10)
+    unlabel_editor.grid(row= 0, column= 0)
+
+    username_editor = Entry(editor)
+    username_editor.grid(row=0, column=1)
+
+    passlabel_editor = Label(editor, text="Password", pady=10)
+    passlabel_editor.grid(row=1, column= 0)
+
+    password_editor = Entry(editor)
+    password_editor.grid(row=1, column=1)
+
+    save = Button(editor, text="Save a record", command=edit)
+    save.grid(row= 3, column= 0, columnspan=2, ipadx= 52)
+
+    #LOOP THROUGH RECORDS-------------------------------------
+    for record in records:
+        username_editor.insert(0, record[0])
+        password_editor.insert(0, record[1])
+
+
 #WIDGETS & GRID----------------------------------------------------
 unlabel = Label(root, text="Username", pady=10)
 unlabel.grid(row= 0, column= 0)
@@ -102,6 +136,9 @@ delete_label.grid(row=5, column=0, pady=20)
 
 delete = Button(root, text="Delete a record", command=delete)
 delete.grid(row= 6, column= 0, columnspan = 2, ipadx=45)
+
+edit = Button(root, text="Edit a record", command=edit)
+edit.grid(row= 7, column= 0, columnspan=2, ipadx= 52)
 
 conn.commit() #commit changes
 conn.close() #close connection
