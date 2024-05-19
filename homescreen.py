697914b3f0fb4134b59d57d2881c5583fada970c
vidhya <vidhiney05@gmail.com>
2024-05-19 14:38:52 +0800

    temporary quote display

--- homescreen.py
+++ homescreen.py
@@ -1,23 +1,25 @@
 from tkinter import *
 import customtkinter
+from PIL import Image, ImageTk
+import random
+import time
 root = Tk()
 root.geometry("1200x700")
 root.title("Home Screen")
 root.configure(bg="#E8D09C")
 root.resizable(False, False)
 
-#BACKGROUND----------------------------------------------------------
-
 
 #FUNCTIONS-----------------------------------------------------------
 
-def quote():
-
-    image = PhotoImage(file="cuba.png")
-
-    image_label = Label(midframe, image=image)
-    image_label.image = image  # prevents garbage collection
-    image_label.grid(row=0, column=1)
+def quote(canvas, images):
+    for img_id, img in images.items():
+        canvas.delete("all")
+        x = random.randint(0, canvas.winfo_width() - img.width())
+        y = random.randint(0, canvas.winfo_height() - img.height())
+        canvas.create_image(x, y, anchor=NW, image=img)
+        canvas.update()
+        time.sleep(5)
 
 
 '''def redirect_todo():
@@ -42,15 +44,26 @@ font_15 = ("Gill Sans MT", 15)
 
 
 #WIDGETS and PACKING-------------------------------------------------
-midframe = Frame(root, padx=20, pady=20, bg="#FFFFFF")
-midframe.pack(expand=True)
+quoteframe = Frame(root, width=700, height=400, bg="#FFFFFF")
+quoteframe.pack(expand=True)
+quoteframe.pack_propagate(False) #prevents resizing according to content / maintains size
 
-quote = Button(midframe, text="quote", fg="navy", relief="flat", command=quote)
-quote.grid(row=0, column=0)
+canvas = Canvas(quoteframe, width=700, height=400, bg="#000000")
+canvas.pack()
 
-'''nagiwagi = PhotoImage(file="nagito.png")
-picbutton = customtkinter.CTkButton(master=midframe, image=nagiwagi, compound="center", bg_color="transparent")
-picbutton.grid(row=1, column=0)'''
+images = {
+    0: ImageTk.PhotoImage(Image.open('Quotes/wp1.png')),
+    1: ImageTk.PhotoImage(Image.open('Quotes/wp2.png')),
+    2: ImageTk.PhotoImage(Image.open('Quotes/wp3.png')),
+    3: ImageTk.PhotoImage(Image.open('Quotes/wp4.png')),
+    4: ImageTk.PhotoImage(Image.open('Quotes/wp5.png')),
+    5: ImageTk.PhotoImage(Image.open('Quotes/wp6.png')),
+    6: ImageTk.PhotoImage(Image.open('Quotes/wp7.png')),
+    7: ImageTk.PhotoImage(Image.open('Quotes/wp8.png')),
+    8: ImageTk.PhotoImage(Image.open('Quotes/wp9.png')),
+    9: ImageTk.PhotoImage(Image.open('Quotes/wp10.png'))
+}
+
 
 #footer
 botframe = Frame(root, bg="#FFFFFF", padx=20, pady=20)
@@ -71,5 +84,6 @@ music_button.grid(row= 1, column= 4)
 flash_button = Button(botframe, text="flashcards", bg= "#FFFFFF")
 flash_button.grid(row= 1, column= 5)
 
+root.after(1000, quote, canvas, images) #start displaying images after 1000ms (1s)
 
 root.mainloop()
