8e9aa04a8933d62ecfe1ab27f714a7b126251e4a
vidhya <vidhiney05@gmail.com>
2024-05-27 16:23:47 +0800

    drafted the middle frame

--- homescreen.py
+++ homescreen.py
@@ -15,25 +15,30 @@ root.resizable(False, False)
 font_30 = ("Gill Sans MT", 30, "bold")
 font_15 = ("Gill Sans MT", 15)
 
+#icon pictures
 todo_pic = PhotoImage(file="./icons/icon1.png")
 pomo_pic = PhotoImage(file="./icons/icon2.png")
-music_pic = PhotoImage(file="./icons/icon3.png")
-scratch_pic = PhotoImage(file="./icons/icon4.png")
+scratch_pic = PhotoImage(file="./icons/icon3.png")
+music_pic = PhotoImage(file="./icons/icon4.png")
 flash_pic = PhotoImage(file="./icons/icon5.png")
 
-pathtodo = "./Mytodo/Meowtodo.py"
+#quotes pictures
+mascotpic = PhotoImage(file="picreg.png")
+trial = PhotoImage(file="piclog.png")
+
+pathtodo = "Meowtodo.py"
 argstodo = '"%s" "%s"' % (sys.executable, pathtodo)
 
 pathpomo = "pomodoro.py"
 argspomo = '"%s" "%s"' % (sys.executable, pathpomo)
 
-pathmusic = "./Mymusic/Meowmusic.py"
+pathmusic = "Meowmusic.py"
 argsmusic = '"%s" "%s"' % (sys.executable, pathmusic)
 
 pathscratch = "scratchpadv2.py"
 argsscratch = '"%s" "%s"' % (sys.executable, pathscratch)
 
-pathflash = "./Myflashcard/Meowflashcard.py"
+pathflash = "Meowflashcard.py"
 argsflash = '"%s" "%s"' % (sys.executable, pathflash)
 
 
@@ -56,6 +61,19 @@ def redirect_flash():
 
 
 #WIDGETS and PACKING-------------------------------------------------
+
+#middle
+midframe = Frame(root, bg="#FFFFFF", padx=20, pady=10, borderwidth=0)
+midframe.pack()
+
+mascot = Label(midframe, image=mascotpic)
+mascot.grid(row=0, column=0, rowspan=2)
+
+quotetitle = Label(midframe, bg="#FFFFFF", font=font_15, text="Wisdom Purrs")
+quotetitle.grid(row=0, column=1)
+
+quotepic = Label(midframe, image=trial)
+quotepic.grid(row=1, column=1)
 
 #footer
 botframe = Frame(root, bg="#FFFFFF", padx=20, pady=10, borderwidth=0)
